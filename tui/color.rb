3a58b89eff1d6ddec2c5eeab81ad306ba4f0467c
Boris Wetzl <boris@z3n.de>
2017-01-25 17:41:55 +0100

    update tui

--- tui/color.rb
+++ tui/color.rb
@@ -13,49 +13,36 @@ class Color
     end
 
     def set!(r = 1.0, g = nil, b = nil)
-        if g == nil && b == nil
-            @r = r
-            @g = r
-            @b = r
-        else
-            @r = r
-            @g = g
-            @b = b
-        end
+        g, b = r, r if (g.nil? && b.nil?)
+        @r, @g, @b = r, g, b
         self
     end
 
     def set_int!(r = 5, g = nil, b = nil)
-        if g == nil && b == nil
-            self.set!(r/5.0)
-        else
-            self.set!(r/5.0, g/5.0, b/5.0)
-        end
+        g, b = r, r if (g.nil? && b.nil?)
+        self.set!(r/5.0, g/5.0, b/5.0)
     end
 
     def to_s
         if @r == @g && @g == @b
             ((@r * 23.0).round + 232).to_s
         else
-            (16 +
-             (@b * 5.0).round +
-             ((@g * 5.0).round * 6) +
-             ((@r * 5.0).round * 36)).to_s
+            (16 + (@b * 5.0).round + ((@g * 5.0).round * 6) + ((@r * 5.0).round * 36)).to_s
         end
     end
 
     def drop_shadow!
         if @r == @g && @g == @b
-            @r   *= 0.7
+            @r *= 0.7
             @g *= 0.7
-            @b  *= 0.7
+            @b *= 0.7
         else
-            @r   -= 0.4
+            @r -= 0.4
             @g -= 0.4
-            @b  -= 0.4
-            @r    = 0 if @r < 0
+            @b -= 0.4
+            @r  = 0 if @r < 0
             @g  = 0 if @g < 0
-            @b   = 0 if @b < 0
+            @b  = 0 if @b < 0
         end
         self
     end
